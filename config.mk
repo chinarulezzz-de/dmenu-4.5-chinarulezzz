785f7e83556a9c90676084a3d145adc3072f51f7
chinarulezzz <drop@chinarulezzz.fun>
2020-08-26 00:14:50 +0300

    config.mk: add BSD support

--- config.mk
+++ config.mk
@@ -1,26 +1,39 @@
 # dmenu version
-VERSION = 4.5
+VERSION=	4.5
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+PREFIX=		/usr/local
+MANPREFIX=	${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# includes
+BASEINC=	-I${.CURDIR}
+BASEINC+=	-I/usr/include
+BASEINC+=	-I/usr/local/include
+X11INC=		-I/usr/include/X11
+X11INC+=	-I/usr/local/include/X11
+FREETYPEINC=	-I/usr/include/freetype2
+FREETYPEINC+=	-I/usr/local/include/freetype2
+
+# libs
+BASELIB=	-L/usr/lib
+BASELIB+=	-L/usr/local/lib
+X11LIB=		-L/usr/lib/X11
+X11LIB+=	-L/usr/local/lib/X11
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
+XINERAMALIBS=	-lXinerama
+XINERAMAFLAGS=	-DXINERAMA
 
 # includes and libs
-INCS = -I${X11INC} -I/usr/include/freetype2
-LIBS = -L${X11LIB} -lX11 -lXft ${XINERAMALIBS}
+INCS=		${BASEINC} ${X11INC} ${FREETYPEINC}
+LIBS=		${BASELIB} ${X11LIB} -lc -lX11 -lXft ${XINERAMALIBS}
 
 # flags
-CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+#CPPFLAGS=	-D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS=	-D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CFLAGS=		-g -std=c99 -pedantic -Wall -Wextra -O0 ${INCS} ${CPPFLAGS}
+#CFLAGS=		-std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS=	-s ${LIBS}
 
 # compiler and linker
-CC = cc
+CC?=		cc
